ea4312d72de89c7f1e9fb6e05eddbe8e1e545fd1
MakerSquare Student <students@themakersquare.com>
2013-09-18 12:51:09 -0500

    Complete basic requirements

--- clashing-hashes.rb
+++ clashing-hashes.rb
@@ -2,7 +2,19 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
+require_relative 'lib/twitter'
 
+# Get request for the root url
 get '/' do
-  'hello world'
+  @title = '<i class="icon-bolt"></i> Clashing Hashes <i class="icon-bolt"></i>'
+  @show_description = true
+  erb :index
+end
+
+# Post request for /twitter
+post '/twitter' do
+  tweet = Twitter::Tweet.new
+  @tweets_list = tweet.tweets
+  @title = '<i class="icon-flag-checkered"></i> Form Submitted <i class="icon-flag-checkered"></i>'
+  erb :twitter
 end
